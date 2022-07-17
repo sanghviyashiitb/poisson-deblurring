eb1cdeadf0f619da3d54b0792fd0bf0de011202e
Yash Sanghvi <sanghviyash95@gmail.com>
2022-07-17 11:28:25 -0400

    Update demo_real.py

--- demo_real.py
+++ demo_real.py
@@ -4,9 +4,12 @@ import argparse
 import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
-from utils.utils_test import rggb_to_rgb, p4ip_wrapper_pad, img_register
+from utils.utils_test import rggb_to_rgb, p4ip_wrapper_pad
+from utils.utils_test import img_register, gray_world_whitebalance, change_whitebalance
 from utils.utils_torch import conv_fft, img_to_tens, scalar_to_tens
 from utils.utils_deblur import pad, crop
+from skimage.metrics import structural_similarity as ssim
+import cv2 as cv2
 
 from models.network_p4ip import P4IP_Net
 
@@ -14,14 +17,15 @@ parser = argparse.ArgumentParser(description='Test on Real Data')
 parser.add_argument('--idx', type=int, default=10, help='index of real data file [0,29]')
 args = parser.parse_args()
 
-DIR = 'data/real_data/lux5'
+DIR = '../P4IP/Python/data/real_data/'
+# DIR = 'data/real_data/'
 MODEL_FILE = 'model_zoo/p4ip_100epoch.pth'
 IDX = args.idx
 IDX_CLEAN = int(IDX/3)
 
 # Load data-files
-y = np.load(DIR+'/cut'+str(IDX)+'.npy') 			# Noisy image
-k = np.load(DIR+'/kernel'+str(IDX)+'.npy') 			# blur kernel
+y = np.load(DIR+'/lux5/cut'+str(IDX)+'.npy') 			# Noisy image
+k = np.load(DIR+'/lux5/kernel'+str(IDX)+'.npy') 			# blur kernel
 # Clipping negative values and normalization of kernel
 y = np.clip(y.astype(np.float32),0,np.inf); k = np.clip(k.astype(np.float32),0,np.inf)
 k /= np.sum(np.ravel(k))
@@ -62,26 +66,36 @@ with torch.no_grad():
 		rggb.append(x_rec)	
 	x_p4ip = rggb_to_rgb(rggb, H, W, MODE)
 	y = rggb_to_rgb(y_list, H, W, MODE)
-den_arr = np.load('data/real_data/denominator_data.npy')
-den = den_arr[IDX_CLEAN]
-x_p4ip = x_p4ip.astype(np.float32)*255/den
-y = y.astype(np.float32)*255/den
-cv2.imwrite('results/p4ip_'+str(IDX)+'.png', x_p4ip )
-cv2.imwrite('results/y_'+str(IDX)+'.png', y )
 
-# Load images in CV2 format for image registration and comparison
-x_p4ip_cv = cv2.imread('results/p4ip_'+str(IDX)+'.png')
-x_gt_cv = cv2.imread(DIR+'_clean/'+str(IDX_CLEAN)+'.png')	# clean image
-y_cv  = cv2.imread('results/y_'+str(IDX)+'.png')
+MODE = 'BGGR' if IDX in [15,20,24] else 'RGGB'
+x_p4ip = rggb_to_rgb(rggb, H, W, MODE); x_p4ip = gray_world_whitebalance(x_p4ip)/2.0
+y = rggb_to_rgb(y_list, H, W, MODE); y = gray_world_whitebalance(y)/2.0
 
-im_true_register, im_estimated =  img_register(x_gt_cv, x_p4ip_cv)
+IDX_CLEAN = int(IDX/3)
+x_gt = cv2.imread(DIR+'lux5_clean/'+str(IDX_CLEAN)+'.png') # clean image
+x_gt = np.flip(x_gt, 2) ## Flip because cv2 loads in BGR format instead of RGB
+x_p4ip_norm = (x_p4ip-np.min(x_p4ip))/(np.max(x_p4ip)-np.min(x_p4ip))
+im_register, _ =  img_register(x_gt, (x_p4ip_norm*255).astype(np.uint8))
+im_estimated, im_register = change_whitebalance(x_p4ip, im_register.astype(np.float32)) 
+y, _ = change_whitebalance(y, im_register.astype(np.float32)) 
+
+
+
+err = im_register-im_estimated
+err_mean = np.sqrt(np.mean(err**2))
+psnr=-20*np.log10(err_mean/255.0)
+ssim_val = ssim(im_register, im_estimated,  multichannel = True ,data_range=255)
+print('PSNR: %0.2f, SSIM: %0.3f'%(psnr,ssim_val))
+
+
 plt.figure(figsize=(10,6))
-plt.subplot(1,3,1); plt.imshow(np.flip(im_true_register, axis=2)); plt.axis('off')
+plt.subplot(1,3,1); plt.imshow(im_register/255.0); plt.axis('off')
 plt.title('True Image, After Registration')
-plt.subplot(1,3,2); plt.imshow(np.flip(y_cv, axis=2)); plt.axis('off')
+
+plt.subplot(1,3,2); plt.imshow(y/255.0); plt.axis('off')
 plt.title('Noisy Blurred Image')
-plt.subplot(1,3,3); plt.imshow(np.flip(im_estimated, axis=2)); plt.axis('off')
+
+plt.subplot(1,3,3); plt.imshow(im_estimated/255.0); plt.axis('off')
 plt.title('Reconstructed Image')
 plt.savefig('results/demo_real.png', bbox_inches='tight', pad_inches=0.05)
 plt.show()
-
